344625516786b920661480b71259d9916ef6275c
bourawi12 <mohamed12bouaziz@gmail.com>
2023-10-02 19:49:57 +0100

    creating a carousel

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let images=["0","1","2"];
+let id=0;
+function next(){
+    if (id==(images.length-1)){
+        console.log("im in if :" ,id);
+        id=0;
+        document.getElementById("im").src=images[id]+".jpg";
+    }else{
+        console.log("im in else :" ,id);
+    id++;
+    document.getElementById("im").src=images[id]+".jpg";
+}
+
+}
+function prev(){
+    if (id==0){
+        console.log("im in if :" ,id);
+        id=(images.length-1);
+        document.getElementById("im").src=images[id]+".jpg";
+    }else{
+        console.log("im in else :" ,id);
+    id--;
+    document.getElementById("im").src=images[id]+".jpg";
+}
+}
+function repete(){
+    
+    setTimeout(() => {
+        next();}, "3000");
+    
+}
